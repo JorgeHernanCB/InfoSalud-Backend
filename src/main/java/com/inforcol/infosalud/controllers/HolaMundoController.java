1ffa699783eb912c5bb1d03becd7002a02a871df
Jorge Castaño <jcastano@inforcol.com>
2024-08-08 16:44:55 -0500

    Solucion metodo saludar

--- src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
+++ src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
@@ -3,6 +3,8 @@ package com.inforcol.infosalud.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -10,10 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "hola")
 public class HolaMundoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HolaMundoController.class);
+
     @Operation(summary = "Metodo de bienvenida")
     @GetMapping("/hola")
     public String holaMundo(@RequestParam(name = "nombre") String nombre) {
-        return "¡Hola " + nombre;
+
+        logger.info("Se esta saludando a: " + nombre);
+
+        return "¡Hola " + nombre + "!";
     }
 
 }
