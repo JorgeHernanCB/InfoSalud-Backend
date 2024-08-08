ab2e4303d9fcfb6b4f4d9831674be17802a20635
Jorge Castaño <jcastano@inforcol.com>
2024-08-08 15:22:34 -0500

    Solucion problema SwaggerConfig

--- src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
+++ src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,8 @@ public class HolaMundoController {
 
     @Operation(summary = "Metodo de bienvenida")
     @GetMapping("/hola")
-    public String holaMundo() {
-        return "¡Hola Mundo!";
+    public String holaMundo(@RequestParam(name = "nombre") String nombre) {
+        return "¡Hola " + nombre;
     }
 
 }
