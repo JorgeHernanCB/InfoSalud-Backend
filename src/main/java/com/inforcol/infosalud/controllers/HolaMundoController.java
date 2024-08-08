8bb33189ea54b62d4981e34714b8b71e96e5c328
juansm1664 <juansm3007@gmail.com>
2024-08-08 16:20:16 -0500

    ConfiguraciónRutas

--- src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
+++ src/main/java/com/inforcol/infosalud/controllers/HolaMundoController.java
@@ -2,11 +2,9 @@ package com.inforcol.infosalud.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping ("/api")
 @Tag(name = "hola")
